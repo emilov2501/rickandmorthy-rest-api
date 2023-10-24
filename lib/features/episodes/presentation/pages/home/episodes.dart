8dfd75b3eedde16aa16cba7c388be88e0cb7fcf6
Eldiiar Temir <etemir@nurtelecom.kg>
2023-10-24 22:25:40 +0600

    implement ui infinity scroll

--- lib/features/episodes/presentation/pages/home/episodes.dart
+++ lib/features/episodes/presentation/pages/home/episodes.dart
@@ -3,8 +3,28 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mbank_testy/features/episodes/presentation/bloc/episode/remote_episode_bloc.dart';
 import 'package:mbank_testy/features/episodes/presentation/widgets/episode_tile.dart';
 
-class Episodes extends StatelessWidget {
+class Episodes extends StatefulWidget {
   const Episodes({super.key});
+
+  @override
+  State<Episodes> createState() => _EpisodesState();
+}
+
+class _EpisodesState extends State<Episodes> {
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        context.read<RemoteEpisodesBloc>().add(GetEpisodesNextEvent());
+      }
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,32 +41,35 @@ class Episodes extends StatelessWidget {
   }
 
   _buildBody() {
-    return BlocBuilder<RemoteEpisodesBloc, RemoteEpisodesState>(
-      builder: (_, state) {
-        if (state.status == RemoteEpisodeStatus.loading) {
-          return const Center(
-            child: CircularProgressIndicator.adaptive(),
-          );
-        }
+    return SafeArea(
+      child: BlocBuilder<RemoteEpisodesBloc, RemoteEpisodesState>(
+        builder: (_, state) {
+          if (state.status == RemoteEpisodeStatus.loading) {
+            return const Center(
+              child: CircularProgressIndicator.adaptive(),
+            );
+          }
 
-        if (state.status == RemoteEpisodeStatus.failure) {
-          return const Center(
-            child: Icon(Icons.refresh),
-          );
-        }
+          if (state.status == RemoteEpisodeStatus.failure) {
+            return const Center(
+              child: Icon(Icons.refresh),
+            );
+          }
 
-        if (state.status == RemoteEpisodeStatus.success) {
-          final episodes = state.episodes;
-          return ListView.builder(
-            itemCount: episodes.length,
-            itemBuilder: (context, index) => EpisodeWidget(
-              episode: episodes[index],
-            ),
-          );
-        }
+          if (state.status == RemoteEpisodeStatus.success) {
+            final episodes = state.episodes;
+            return ListView.builder(
+              controller: _scrollController,
+              itemCount: episodes.length,
+              itemBuilder: (context, index) => EpisodeWidget(
+                episode: episodes[index],
+              ),
+            );
+          }
 
-        return const SizedBox.shrink();
-      },
+          return const SizedBox.shrink();
+        },
+      ),
     );
   }
 }
