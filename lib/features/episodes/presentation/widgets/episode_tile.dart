3e22ec73c787189f13fd1e4e56dd09b63728d080
Eldiiar Temir <etemir@nurtelecom.kg>
2023-10-24 20:16:20 +0600

    add episode widget

---
+++ lib/features/episodes/presentation/widgets/episode_tile.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:mbank_testy/features/episodes/domain/entities/episode.dart';
+
+class EpisodeWidget extends StatelessWidget {
+  final EpisodeEntity episode;
+
+  const EpisodeWidget({super.key, required this.episode});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsetsDirectional.only(
+          start: 14, end: 14, bottom: 14, top: 14),
+      child: Row(
+        children: [
+          Text('${episode.name}'),
+        ],
+      ),
+    );
+  }
+}
