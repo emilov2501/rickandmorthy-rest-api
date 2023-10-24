cf5f42d0b46861277ff0b10a78432292d9227f74
Eldiiar Temir <etemir@nurtelecom.kg>
2023-10-24 15:54:32 +0600

    implement getEpisode method

--- lib/features/episodes/data/repository_impl/episode_repository_impl.dart
+++ lib/features/episodes/data/repository_impl/episode_repository_impl.dart
@@ -1,11 +1,32 @@
+import 'dart:io';
+import 'package:dio/dio.dart';
 import 'package:mbank_testy/core/resource/data_state.dart';
+import 'package:mbank_testy/features/episodes/data/data_source/remote/episodes_api_service.dart';
 import 'package:mbank_testy/features/episodes/data/models/episode.dart';
 import 'package:mbank_testy/features/episodes/domain/repository/episode_repository.dart';
 
 class EpisodeRepositoryImpl implements EpisodeRepository {
+  final EpisodesApiService _episodesApiService;
+
+  EpisodeRepositoryImpl(this._episodesApiService);
+
   @override
-  Future<DataState<List<EpisodeModel>>> getEpisodes() {
-    // TODO: implement getEpisodes
-    throw UnimplementedError();
+  Future<DataState<List<EpisodeModel>>> getEpisodes() async {
+    try {
+      final httpResponse = await _episodesApiService.getEpisodes();
+
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(DioException(
+          error: httpResponse.response.statusMessage,
+          response: httpResponse.response,
+          type: DioExceptionType.badResponse,
+          requestOptions: httpResponse.response.requestOptions,
+        ));
+      }
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
   }
 }
