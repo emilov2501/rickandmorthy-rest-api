67c6a87e326abea74d5ac612a89f746253c523c2
Eldiiar Temir <etemir@nurtelecom.kg>
2023-10-25 15:14:43 +0600

    update packages

--- lib/config/theme/app_themes.dart
+++ lib/config/theme/app_themes.dart
@@ -9,9 +9,15 @@ ThemeData theme() {
 
 AppBarTheme appBarTheme() {
   return const AppBarTheme(
-      color: Colors.white,
-      elevation: 0,
-      centerTitle: true,
-      iconTheme: IconThemeData(color: Color(0XFF8B8B8B)),
-      titleTextStyle: TextStyle(color: Color(0XFF8B8B8B), fontSize: 18));
+    color: Colors.white,
+    elevation: 0,
+    centerTitle: true,
+    iconTheme: IconThemeData(
+      color: Color(0XFF8B8B8B),
+    ),
+    titleTextStyle: TextStyle(
+      color: Color(0XFF8B8B8B),
+      fontSize: 18,
+    ),
+  );
 }
